2d1e7d10c4cb94a3c34b21d3b9afc3c2bfd2c5ef
Stephanvdriel <sp.vandriel@student.avans.nl>
2025-10-13 22:29:37 +0200

    Delete TODO in Routing

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -55,20 +55,6 @@ fun Application.configureRouting(config: JWTConfig) {
                 call.respondText("hello, $username! The token expires after $expiresAt ms.")
             }
         }
-
-
-
-
-//        // HTTP GET route for the root path ("/"), responds with "Hello World!" for now.
-//        // TODO: Replace with actual content later, usage and route endpoints.
-//        get("/") {
-//            call.respondText("Hello World!")
-//        }
-//        // HTTP GET route for the tail card pattern, this acts as a catch-all for any undefined routes.
-//        // It redirects to the root path ("/").
-//        get("{...}") {
-//            call.respondRedirect("/", permanent = false)
-//        }
     }
 }
 
