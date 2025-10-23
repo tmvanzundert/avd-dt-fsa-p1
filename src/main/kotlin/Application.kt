f03766fb719c50d215b6542a350c895c6f03c6bf
Stefan de Gier <si.degier@student.avans.nl>
2025-10-23 15:56:11 +0200

    First try implementation of database

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -3,14 +3,30 @@ package com.example
 import io.ktor.server.application.*
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import org.jetbrains.exposed.v1.jdbc.*
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+    embeddedServer(Netty, port = 8085, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
 }
 
 fun Application.module() {
+    configureDatabase()
+
     configureSerialization()
     configureRouting()
     configureStatusPages()
+}
+
+fun configureDatabase() {
+    try {
+        Database.connect(
+            "jdbc:mariadb://localhost:3306/plugandplay",
+            user = "dbuser",
+            password = "LkC9STj5n6bztQ"
+        )
+    }
+    catch (e: Exception) {
+        throw Exception("Could not connect to the database. Error details: '${e.message}'")
+    }
 }
