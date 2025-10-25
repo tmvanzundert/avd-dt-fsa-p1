db801ac169f11ba1c4beea032a1d9283187d7e81
tmvanzundert <tm.vanzundert@student.avans.nl>
2025-10-25 21:06:06 +0200

    Database designs (#25)
    * Added the database designs for the API
    
    * Fixes in DDL
    
    * Updated the vehicle in the DDL
    
    ---------
    
    Co-authored-by: Stefan de Gier <si.degier@student.avans.nl>

---
+++ database/DCL.sql
@@ -0,0 +1,26 @@
+-- DCL: Used for managing permissions and access control
+
+-- Drop existing users if they exist
+DROP USER IF EXISTS 'dbuser'@'localhost';
+DROP USER IF EXISTS 'svandriel'@'%';
+DROP USER IF EXISTS 'sdegier'@'%';
+DROP USER IF EXISTS 'tvanzundert'@'%';
+
+-- Service accounts
+CREATE USER IF NOT EXISTS 'dbuser'@'localhost' IDENTIFIED BY 'LkC9STj5n6bztQ';
+
+-- Grant privileges to service account
+GRANT ALL PRIVILEGES ON plugandplay.* TO 'dbuser'@'localhost';
+
+-- Developers
+CREATE USER IF NOT EXISTS 'svandriel'@'%' IDENTIFIED BY 'xuW7yab5y8mmDx';
+CREATE USER IF NOT EXISTS 'sdegier'@'%' IDENTIFIED BY 'kESbWFwGLY9Dqo';
+CREATE USER IF NOT EXISTS 'tvanzundert'@'%' IDENTIFIED BY 'TVUPgwBm9fL7ST';
+
+-- Grant privileges to developers
+GRANT ALL PRIVILEGES ON plugandplay.* TO 'svandriel'@'%';
+GRANT ALL PRIVILEGES ON plugandplay.* TO 'sdegier'@'%';
+GRANT ALL PRIVILEGES ON plugandplay.* TO 'tvanzundert'@'%';
+
+-- reload privileges
+FLUSH PRIVILEGES;
