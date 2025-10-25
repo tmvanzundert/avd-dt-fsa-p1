745613348dc964474e621cbf8f98d3fc60a992b6
Stefan de Gier <si.degier@student.avans.nl>
2025-10-25 17:49:34 +0200

    Added the database designs for the API

---
+++ src/main/resources/Designs/DDL.sql
@@ -0,0 +1,54 @@
+CREATE DATABASE IF NOT EXISTS plugandplay;
+
+USE plugandplay;
+
+SET FOREIGN_KEY_CHECKS=0;
+DROP TABLE IF EXISTS `User`;
+DROP TABLE IF EXISTS `Vehicle`;
+SET FOREIGN_KEY_CHECKS=1;
+
+CREATE TABLE
+    IF NOT EXISTS `User` (
+        `id` BIGINT AUTO_INCREMENT PRIMARY KEY,
+        `name` VARCHAR(255) NOT NULL,
+        `role` ENUM (
+            "USER",
+            "ADMIN",
+            "NULL"
+        ) DEFAULT 'NULL',
+        `phone` VARCHAR(20) NOT NULL,
+        `password` VARCHAR(255) NOT NULL,
+        `email` VARCHAR(255) NOT NULL,
+        `rating` FLOAT,
+        `createdAt` DATETIME,
+        `birthDate` DATETIME,
+        `driverLicenseNumber` VARCHAR(50) NOT NULL,
+    );
+
+
+CREATE TABLE
+    IF NOT EXISTS `Vehicle` (
+        `id` BIGINT AUTO_INCREMENT PRIMARY KEY,
+        `make` VARCHAR(50),
+        `model` VARCHAR(50),
+        `year` INT,
+        `category` VARCHAR(50),
+        `seats` INT,
+        `range` DOUBLE,
+        `beginOdometer` INT,
+        `endOdometer` INT,
+        `licensePlate` VARCHAR(20),
+        `status` ENUM (
+            "AVAILABLE",
+            "RENTED",
+            "MAINTENANCE"
+            "NULL"
+        ) DEFAULT "NULL"
+        `location` VARCHAR(100),
+        `kilometerRate` DOUBLE,
+        `photoPath` TEXT,
+        `beginReservation` DATETIME,
+        `endReservation` DATETIME,
+
+        ADD FOREIGN KEY (`ownerId`) REFERENCES `User`(`id`) ON DELETE CASCADE
+    );
